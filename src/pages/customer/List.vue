5cdbb38cddaa40a2cc83ee209aaf3ce1c43c9279
yangqixin123 <2370857698@qq.com>
2020-01-03 16:01:07 +0800

    customerManage

--- src/pages/customer/List.vue
+++ src/pages/customer/List.vue
@@ -98,7 +98,7 @@ export default {
           type: 'warning'
         }).then(() => {
           //调用后台接口完成删除操作
-          let url="http://134.175.154.93:6677/customer/deleteById?id"+id;
+          let url="http://134.175.154.93:6677/customer/deleteById?id="+id;
           request.get(url).then((response)=>{
               //刷新数据
                 this.loadData();
